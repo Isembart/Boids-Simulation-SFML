82589f02c0dcf77696349998f872a1681aaa6a90
Isembart <maurycyfar@gmail.com>
2023-11-21 03:36:29 +0100

    added tests folder and cmakefile

---
+++ tests/gameObjectTest.cpp
@@ -0,0 +1,10 @@
+//test for gameObject.cpp
+#include <SFML/Graphics.hpp>
+#include <gtest/gtest.h>
+
+#include "typedefs.hpp"
+#include "gameObject.hpp"
+
+TEST(gameObjectTest, gettersAndSetters) {
+    EXPECT_TRUE(true);
+}
